16e1002aded0aac227a5828f24a4496624735f11
Shameek Poddar <shameek.poddar@gmail.com>
2017-09-10 17:21:33 -0400

    Add Redux

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 import PropertyList from './ItemList.js'
 import { decamelize } from './helpers.js'
 
 const isGroup = (object) => !!object.containing_object
 
-class App extends Component {
-  render() {	
-	const groups = this.props.schema.filter(isGroup)
-	const generalInfo = this.props.schema.filter((obj) => !isGroup(obj))  
+const App = ({ schema, dispatch }) => {	
+	const groups = schema.filter(isGroup)
+	const groupNames = groups.map((group) => group.name).concat("general_info")
+	const generalInfo = schema.filter((obj) => !isGroup(obj))  
 
     return (
       <div className="App">
-			{groups.map((group) => (
-				<div>
-					{decamelize(group.name)}: <PropertyList items={group.containing_object.properties} />
-				</div>
-			))}
-			
-			General Info: <PropertyList items={generalInfo} />
+			<div id="sidebar">
+				{groupNames.map((name) => (<p data-name={name}>{decamelize(name)}</p>))}
+			</div>
+			<div id="properties">
+				{groups.map((group) => (
+					<div>
+						{decamelize(group.name)}: <PropertyList items={group.containing_object.properties} />
+					</div>
+				))}
+				General Info: <PropertyList items={generalInfo} />
+			</div>
       </div>
     );
   }
-}
 
-export default App;
+export default connect((state) => state)(App);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
 import * as schema from './schema'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App schema={schema} />, document.getElementById('root'));
+//Create Redux store
+let reducer = (state = { schema }, action) => state
+
+let store = createStore(reducer, { schema }, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+console.log(store.getState())
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>, 
+	document.getElementById('root')
+);
+	
 registerServiceWorker();
